fd441bf2363d8ad45b9ccfb04e8eae892919db03
Arnav <arnchkv@gmail.com>
2023-01-11 14:05:48 +0530

    chap 9 state and setState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
 import Person from "./Components/Person";
 // import Man from "./Components/Man";
-import Demo from "./Components/Demo";
+// import Demo from "./Components/Demo";
 
 function App() {
   return (
     <div className="App">
-      <Demo num="1"></Demo>
       <Person name="ABC" age="20" />
-      <Demo num="2"></Demo>
-      {/* <Man /> */}
-      <Person name="DEF" age="22" />
-      <Demo num="3"></Demo>
-      <Person name="Ichiki" />
     </div>
   );
 }
--- src/Components/Person.js
+++ src/Components/Person.js
@@ -1,25 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import Man from './Man';
 
-class Person extends Component{
-    constructor(props){
+class Person extends Component {
+    constructor(props) {
         super(props);
         this.props = props;
+        this.state = {
+            name: "ABC",
+            age: 25
+        }
     }
 
-    render(){
+    nameChangeHandler() { 
+        console.log(this.state)
+        this.setState({name: "vddfee", age: 43}, () => {
+            console.log(this.state)
+        })
+    }
+
+    render() {
         // return(
         //     <div>
         //         <h3>Name:</h3><h3>ABC</h3>
         //         <h3>Age</h3><h3>11 years</h3>
         //         <Man />
         //     </div>
-            
+
         // );
-        return(
+        return (
             <div>
                 <h1>
-                    <i>Dear {this.props.name}, age {this.props.age} years Welcome</i>
+                    <i>Dear {this.state.name}, {this.state.age} years Welcome</i>
+                    <br />
+                    <button onClick={this.nameChangeHandler.bind(this)}>Click Me</button>
                 </h1>
             </div>
         );
